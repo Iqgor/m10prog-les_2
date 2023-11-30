01a4a6dc5aaccd92dd3add9815070c724cfb29b0
Iqgor <herrebrughigor@gmail.com>
2023-11-30 09:23:08 +0100

    Einde les 1

--- public/index.php
+++ public/index.php
@@ -1,19 +1,62 @@
 <?php
 
-Class Auto{
-    private $name;
-    private $color="red";
+Class Bag{
+    private int $compartment;
+    private string $colour;
+    private int $storage;
+    public int $items = 0;
 
- 
-    function set_name($name){
-        $this->name = $name;
-        $this->get_name();
+
+
+    public function __construct(string $newColour, int $newCompartment){
+        $this->compartment = $newCompartment;
+        $this->colour = $newColour;
+        $this->storage = $this->compartment * 4;
+
+
+
     }
     
-    function get_name(){
-        return $this->name;
+    public function changeColour(string $newColour){
+        $this->colour = $newColour;
+        return $this->colour;
     }
+
+    public function addItem(){
+        if($this->items < $this->storage){
+            $this->items++;
+            return $this->items;
+        }
+        die("De tas is vol");
+        
+    }    
+
+    public function get_compartment(){
+        return $this->compartment;
+    }
+
+    public function get_colour(){
+        return $this->colour;
+    }
+    public function get_items(){
+        return $this->items;
+    }
+
 }
 
-$eersteAuto = new Auto();
-$eersteAuto->set_name("ferrari");
+
+$eastpack = $bag = new Bag("black",2);
+echo "Er is/zijn " .$bag->get_compartment() . " vak(ken). <br>";
+
+$eastpack->changeColour("rood");
+
+$bag->addItem();
+$bag->addItem();
+$bag->addItem();
+$bag->addItem();
+
+echo $bag->get_items() . "<br>";
+
+
+echo "De tas is ". $eastpack->get_colour();
+
