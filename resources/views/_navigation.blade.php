9c91365e9c8b430125950dc7f9e93d8aeb2bef40
Iqgor <herrebrughigor@gmail.com>
2023-12-14 09:33:20 +0100

    les 3 af

--- resources/views/_navigation.blade.php
+++ resources/views/_navigation.blade.php
@@ -1,5 +1,5 @@
 <ul class="flex">
-    <li class="mr-3 px-4 px-2  rounded-sm @if(Route::currentRouteName() == 'welcome') @else bg-blue @endif"><a href="{{route('mensen.index')}}">Mensen</a></li>
+    <li class="mr-3 px-4 px-2  rounded-sm @if(Route::currentRouteName() == 'mensen') bg-gray @else bg-blue @endif"><a href="{{route('mensen.index')}}">Mensen</a></li>
     <li class="mr-3 px-4 px-2 bg-blue rounded-sm"><a href="{{route('about.me')}}">About</a></li>
     <li class="mr-3 px-4 px-2 bg-blue rounded-sm">Test</li>
     <li class="mr-3 px-4 px-2 bg-blue rounded-sm">Test</li>
---
+++ resources/views/mensen.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+
+    <link rel="stylesheet" href="/dist/output.css">
+</head>
+
+<body class="antialiased">
+    <header>
+        @include('./_navigation')
+    </header>
+    <nav>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque enim, iusto nostrum dicta, cupiditate a veritatis eligendi saepe qui sapiente ipsam fuga quam minima accusamus sit quidem laborum obcaecati accusantium!
+    </nav>
+    <footer>
+        <p>&#169; Igor Herrebrugh</p>
+    </footer>
+</body>
+
+</html>
